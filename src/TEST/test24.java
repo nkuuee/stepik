ecc13f81b3bbca68b1c2828765bf513beaa42bfa
New <nkuuee@bk.ru>
2024-03-06 16:06:39 +0300

    0

--- src/TEST/test24.java
+++ src/TEST/test24.java
@@ -1,7 +1,14 @@
 package TEST;
 
+import java.util.Scanner;
+
 public class test24 {
     public static void main(String[] args) {
-
+        double number = new Scanner(System.in).nextDouble();
+        if (number % 7 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
